518bface166dd4698c9689b3aa82642a7fc650ad
daria425 <darianaumova5@gmail.com>
2024-07-14 16:58:00 +0300

    add method to sort by postcode

--- ai_api/src/services/AI_Service.py
+++ ai_api/src/services/AI_Service.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv
 import vertexai
 from abc import ABC, abstractmethod
-from vertexai.generative_models import GenerativeModel
+from vertexai.generative_models import (
+    Content,
+    FunctionDeclaration,
+    Part,
+    Tool, GenerativeModel
+)
 from config.vertexai_config import vertexai_project_location, vertex_ai_project_id
 load_dotenv()
 
@@ -18,6 +23,49 @@ class AI_Service(ABC):
             raise ValueError(f"Unsupported model source: {model_source}")
         
 class VertexAI_Service(AI_Service):
-    @abstractmethod
-    def use_model(self):
-        pass
+    def __init__(self, model_source, model_name):
+        super().__init__(model_source, model_name)
+    
+    @staticmethod
+    def create_user_prompt(prompt_text):
+        user_prompt = Content(
+            role="user",
+            parts=[
+                Part.from_text(prompt_text),
+            ],
+        )
+        return user_prompt
+    
+    @staticmethod
+    def create_func_declaration(func_name, func_description, func_params):
+        func = FunctionDeclaration(
+            name=func_name,
+            description=func_description,
+            parameters=func_params
+        )
+        return func
+    
+    def create_tool(self, function_dictionaries):
+        function_declarations=[]
+        for dictionary in function_dictionaries:
+            func_declaration=self.create_func_declaration(dictionary["func_name"], dictionary["func_description"], dictionary["func_params"])
+            function_declarations.append(func_declaration)
+        tool = Tool(function_declarations=function_declarations)
+        return [tool]
+    
+    def get_model_response(self, prompt_text, generation_config, use_tool=False, function_dictionaries=[{}]):
+        if use_tool and not function_dictionaries:
+            raise ValueError("If use_tool is set to True, function_dictionaries must be provided.")
+        tools=[]
+        if use_tool:
+            tools=self.create_tool(function_dictionaries)
+        user_prompt=self.create_user_prompt(prompt_text)
+        response = self.model.generate_content(
+        user_prompt,
+        generation_config=generation_config,
+        tools=tools,
+
+)
+        response_function_call_content = response.candidates[0].content.parts[0].to_dict()
+        organizations=response_function_call_content["function_call"]["args"]["organizations"]
+        print(organizations)
--- ai_api/src/test.py
+++ ai_api/src/test.py
@@ -1,2 +1,57 @@
-from services.AI_Service import AI_Service
-service=AI_Service("vertexai", "gemini-1.5-flash-001")
+from services.AI_Service import VertexAI_Service
+import json
+
+# Function to read JSON data from file
+def read_json_file(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+def create_input_text(option):
+    website=option["Website"]
+    if option["Local / National"]=="National":
+        location=option["Local / National"]
+    else:
+        location=option["Postcode"]
+    description=option["Short text description"]
+    name=option["Name"]
+    result={"website": website, "location": location, "description": description, "name": name}
+    return result
+
+file_path="/home/vboxuser/repos/ai_signposting/ai_api/data/sample_option_messages_local.json"
+options=read_json_file(file_path)
+option_summaries=[create_input_text(option) for option in options]
+list_items=json.dumps(option_summaries)
+user_postcode="PL25 5PQ"
+user_prompt=f"""Please sort the list of python dictionaries from closest to furthest from the postcode {user_postcode} based on the value of the location key.
+    Assume all postcodes are within the UK. Include only the sorted dictionaries in your response, without any additional information or code.
+    Dictionaries to sort: 
+    {list_items}
+    """
+sort_by_postcode_func_dictionary={
+    "func_name": "sort_items_by_postcode", 
+    "func_description": "Sorts the entries by distance from closest to furthest from a given postcode", 
+    "func_params":{
+      "type": "object",
+      "properties": {
+          "organizations": {
+              "type": "array",
+              "description": "A list of information on organizations",
+              "items": {
+                  "description": "A single organization",
+                  "type": "object",
+                  "properties": {
+                      "name": {"type": "string", "description": "Organization name"},
+                      "description": {"type": "string", "description": "A short text description of the organization"},
+                       "website": {"type": "string", "description": "the website of the organization"},
+                      "location": {"type": "string", "description": "The postcode of the organization to use for sorting"},
+                  },
+                  "required": ["website", "location", "name", "description"],
+              },
+          },
+      },
+      "required": ["organizations"],
+  }
+}
+service=VertexAI_Service("vertexai", "gemini-1.5-flash-001")
+service.get_model_response(user_prompt, {"temperature":0}, use_tool=True, function_dictionaries=[sort_by_postcode_func_dictionary])
